ca8c66128b909c7262b4635e9b3e46f496051620
sujitsuwal <sujit.solace@gmail.com>
2024-04-08 08:32:28 +0545

    Connection to MONGODB

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+
+const app = express();
+
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (err) => console.log('Error connecting to MongoDB: ', err))
+        throw  new Error("MongoDB Connection unsuccessful")
+        
+        app.listen(process.env.PORT, () => 
+            console.log(`App is listening on port: ${process.env.PORT}`)
+        )
+
+    } catch (error) {
+          console.error("ERROR:", error)
+    }
+})()
+*/
